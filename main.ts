194709150f4ed41419aa82b9bc451c5ced6d6c29
marxmake <448191786@qq.com>
2021-01-13 16:24:16 +0800

    Signed-off-by: marxmake <448191786@qq.com>

--- main.ts
+++ main.ts
@@ -1,8 +1,2 @@
-iWall.iWall_Init()
-iWall.iWall_SensorInit(iWall.SENSOR.COLOR)
-let c1 = iWall.iWall_createCharacter(characterType.NAME.ROCKET_WITH_FLAME, 0, 0)
-let TXT1 = iWall.iWall_createTextFrame(0, 14, iWall.FONT.ASCII_5X5, 20)
 basic.forever(function () {
-    iWall.iWall_textSetText(TXT1, convertToText(iWall.iWall_SensorGet(iWall.SENSOR_DATA.COLOR_B)), 0xff0000)
-    basic.pause(500)
 })
